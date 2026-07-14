020dc706e01c1d039e7d426858f534f9b4ad4eab
Lucas Horton <lucas@lucashorton.com>
2026-07-13 23:55:46 -0500

    adding Gnome

--- config/modules/gnome.nix
+++ config/modules/gnome.nix
@@ -4,9 +4,25 @@
   services.displayManager.gdm.enable = true;
   services.desktopManager.gnome.enable = true;
 
-  services.gnome.core-apps.enable = false;
+  services.gnome.core-apps.enable = true;
   services.gnome.core-developer-tools.enable = false;
   services.gnome.games.enable = false;
   environment.gnome.excludePackages = with pkgs; [ gnome-tour gnome-user-docs ];
-
+  environment.systemPackages = with pkgs; [
+    gnomeExtensions.alphabetical-app-grid
+    gnomeExtensions.appindicator
+  ];
+  programs.dconf.profiles.user.databases = [
+    {
+      lockAll = true; # prevents overriding
+      settings = {
+        "org/gnome/desktop/interface" = {
+          accent-color = "teal";
+        };
+        "org/gnome/desktop/input-sources" = {
+          xkb-options = [ "ctrl:nocaps" ];
+        };
+      };
+    }
+  ];
 }
