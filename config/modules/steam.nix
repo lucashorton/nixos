9a87ac4a55298a075918d15841bcd75366c29589
Lucas Horton <lucas@lucashorton.com>
2026-07-28 17:58:09 -0500

    removing Steam session

--- config/modules/steam.nix
+++ config/modules/steam.nix
@@ -1,12 +1,10 @@
 {
   programs.gamescope = {
     enable = true;
-    capSysNice = true; # Required for proper real-time thread scheduling
   };
 
   programs.steam = {
     enable = true;
-    gamescopeSession.enable = true; # Adds a Gamescope session to your display manager
   };
 
   programs.gamemode.enable = true; # Optional but recommended for better gaming performance
